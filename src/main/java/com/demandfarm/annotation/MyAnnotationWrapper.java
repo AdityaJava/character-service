7201a83aa38a2b4886f1d7d1141f4be45df744e5
Aditya <adityadudhal97@gmail.com>
2023-11-21 10:07:31 +0530

    Added ProceedingJoinPoint

--- src/main/java/com/demandfarm/annotation/MyAnnotationWrapper.java
+++ src/main/java/com/demandfarm/annotation/MyAnnotationWrapper.java
@@ -1,8 +1,8 @@
 package com.demandfarm.annotation;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
@@ -11,13 +11,17 @@ import org.springframework.stereotype.Component;
 public class MyAnnotationWrapper {
 
     @Pointcut("within(MyAnnotation) || @annotation(MyAnnotation)")
-    public void beforeMyAnnotationInterceptor(){
+    public void beforeMyAnnotationInterceptor() {
 
     }
 
     @Around("beforeMyAnnotationInterceptor()")
-    public void advice(){
-        System.out.println("Annotation working");
+    public void advice(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.out.println("Before Start of method");
+        // Proceed with the execution of the intercepted method
+        Object result = joinPoint.proceed();
+        System.out.println("After End of method");
+
     }
 
 }
